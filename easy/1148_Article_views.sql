caf9eab1cb244969511702ed08349dddd3a9cc76
Chinki Raj <164533712+Chinki021@users.noreply.github.com>
2026-05-05 17:53:09 +0530

    Enhance SQL with comments on article views logic
    Added comments explaining the approach to find users who viewed their own content.

--- easy/1148_Article_views.sql
+++ easy/1148_Article_views.sql
@@ -1,5 +1,9 @@
 -- Problem: Article Views
 -- Link: https://leetcode.com/problems/article-views-i/?envType=study-plan-v2&envId=top-sql-50
+## Approch
+-- Find users who viewed their own content:
+-- Filter rows where author_id = viewer_id,
+-- then use DISTINCT to return unique author_ids as id.
 
 
 select  distinct author_id  as id from Views 
